acfd5a63f1ec47c4967725ac5338e1ec1cb5b3dc
zhelly-dev <149506681+zhelly-dev@users.noreply.github.com>
2025-02-08 12:33:08 -0500

    Add banlist

--- main.go
+++ main.go
@@ -3,44 +3,87 @@ package main
 import (
 	"fmt"
 	"log"
-	"flag"
 	"time"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"flag"
 
 	"github.com/shirou/gopsutil/process"
 )
 
-func main() {
-	name := flag.String("name", "", "Process name")
-	blockTime := flag.Int("time", 1, "Time to block the process")
-	flag.Parse()
-	*blockTime *= 60
+type BanEntry struct {
+	Name string
+	BanTime int
+}
 
-	for *blockTime <= 0 {
-		for {
-			processes, err := process.Processes()
-			if err != nil {
-				log.Fatalf("Error retrieving processes: %v", err)
-			}
+func loadBanList(filename string) ([]BanEntry, error) {
+	var banList []BanEntry
+	file, err := os.Open(filename)
+	if err != nil {return nil, err}
+	defer file.Close()
 
-			for _, p := range processes {
-				aname, err := p.Name()
-				if err != nil {
-					continue
-				}
-
-				if aname == *name {
-					pid := p.Pid
-					fmt.Printf("Killing process: %s (PID: %d)\n", *name, pid)
-					if err := p.Kill(); err != nil {
-						log.Printf("Failed to kill process %d: %v", pid, err)
-					} else {
-						fmt.Printf("Successfully killed process %d\n", pid)
-					}
-				}
-			}
-			time.Sleep(1 * time.Second)
-			*blockTime--;
-			fmt.Println(*blockTime)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+		if len(parts) != 2 {
+			continue
 		}
+		
+		processName := parts[0]
+		banTime, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Printf("Invalid ban time for process %s: %v",  processName, err)
+			continue
+		}
+		banList = append(banList, BanEntry{Name: processName, BanTime: banTime * 60})
+	}
+	if err := scanner.Err(); err != nil {return nil, err}
+	return banList, nil
+}
+
+func main() {
+	banlistFile := flag.String("file", "", "Banlist")
+	flag.Parse()
+	banList, err := loadBanList(*banlistFile)
+	if err != nil {log.Fatalf("Error loading banlist file: %v", err)}
+
+	for _, entry := range banList {
+		go func(entry BanEntry) {
+			blockProcess(entry.Name, entry.BanTime)
+		}(entry)
+	}
+
+	select {}
+}
+
+func blockProcess(processName string, banTime int) {
+	for banTime > 0 {
+		processes, err := process.Processes()
+		if err != nil {
+			log.Printf("Error retrieving processes: %v", err)
+			return
+		}
+
+		for _, p := range processes {
+			aname, err := p.Name()
+			if err != nil {continue}
+
+			if aname == processName {
+				pid := p.Pid
+				fmt.Printf("Blocking process: %s (PID: %d) for %d seconds\n", processName, pid, banTime)
+				if err := p.Kill(); err != nil {
+					log.Printf("Failed to kill process %d: %v", pid, err)
+				} else {
+					fmt.Printf("Successfully blocked process %d\n", pid)
+				}
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+		banTime--
+		fmt.Printf("Time remaining for %s: %d seconds\n", processName, banTime)
 	}
 }
